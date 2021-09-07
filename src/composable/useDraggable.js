deb5dd07202de5f226e4014d342ba55b16344bdf
ippatiev_ag <alexanderippatiev24@gmail.com>
2021-09-07 12:47:00 +0900

    add vue.config.js

--- src/composable/useDraggable.js
+++ src/composable/useDraggable.js
@@ -1,12 +1,8 @@
 export const useDraggable = () => {
   const getNextElement = (cursorPosition, currEl) => {
-    // Получаем объект с размерами и координатами
     const currElCoord = currEl.getBoundingClientRect();
-    // Находим вертикальную координату центра текущего элемента
     const currElCenter = currElCoord.y + currElCoord.height / 2;
 
-    // Если курсор выше центра элемента, возвращаем текущий элемент
-    // В ином случае — следующий DOM-элемент
     const nextElement =
       cursorPosition < currElCenter ? currEl : currEl.nextElementSibling;
 
