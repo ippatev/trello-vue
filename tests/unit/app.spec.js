c59bc5049560d9c53cecf8542fe4bf3d0024cbce
ippatiev_ag <alexanderippatiev24@gmail.com>
2021-09-09 19:10:17 +0900

    update app.spec.js

--- tests/unit/app.spec.js
+++ tests/unit/app.spec.js
@@ -4,10 +4,14 @@ import TaskList from "@/components/TaskList.vue";
 import TaskListItem from "@/components/TaskListItem.vue";
 
 describe("App.vue", () => {
-  it("title", () => {
-    const wrapper = mount(App);
+  const wrapper = mount(App);
+  it("tasks", () => {
     expect(wrapper.find(".tasks")).toBeTruthy();
+  });
+  it("task-list component", () => {
     expect(wrapper.findAllComponents(TaskList)).toBeTruthy();
+  });
+  it("task-list-item components equal 3", () => {
     expect(wrapper.findAllComponents(TaskListItem)).toHaveLength(3);
   });
 });
